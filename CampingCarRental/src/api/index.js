cadd05f54415a88abfbbe30d5509caa0303e0915
Crazyraccoon <e01026ht@gmail.com>
2026-05-19 16:10:51 +0900

    나는 오동근

--- CampingCarRental/src/api/index.js
+++ CampingCarRental/src/api/index.js
@@ -4,16 +4,4 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
 });
 
-// Add a request interceptor to add JWT token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
 export default api;
