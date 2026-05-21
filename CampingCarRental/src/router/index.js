7bed6ad8fe927aad932942e8197a2ec453ba37be
pandanam <pandanam739@gmail.com>
2026-05-21 12:31:54 +0900

    python 백엔드용으로 수정

--- CampingCarRental/src/router/index.js
+++ CampingCarRental/src/router/index.js
@@ -29,16 +29,13 @@ const router = createRouter({
     },
   ],
 })
+router.beforeEach((to, from) => {
+  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'
 
-// Simple navigation guard to check token
-router.beforeEach((to, from, next) => {
-  const isAuthenticated = !!localStorage.getItem('token')
-  if ((to.name === 'main' || to.name === 'rental' || to.name === 'mypage') && !isAuthenticated) {
-    next({ name: 'auth' })
-  } else if (to.name === 'auth' && isAuthenticated) {
-    next({ name: 'main' })
-  } else {
-    next()
+  // 로그인을 안 했는데 메인 페이지로 가려고 하면 가로막기
+  if (to.path === '/main' && !isLoggedIn) {
+    alert('로그인이 필요한 페이지입니다.')
+    return '/login'
   }
 })
 
